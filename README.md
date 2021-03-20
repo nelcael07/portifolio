199b19fb1473e0fa758c7600693fc6590f0bb54d
Nelcael <nelcaif@gmail.com>
2021-03-20 19:47:19 -0300

    :package: menu

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # portifolio
+
 portfolio feito em HTML, CSS e JAVA SCRIPT
 
 ## tarefas
+
 O controle de tarefas desse projetoserá realizado no github
 
 ## icones //isso se refere a icones que seram carregados no git hub para melhor explicação
-:package: nova funcionalidade
-:up: atualização
-:beatle: correção de bug
-:checkered_flag: release
+
+-  :package: nova funcionalidade
+-  :up: atualização
+-  :beetle: correção de bug
+-  :checkered_flag: release
---
+++ script.js
@@ -0,0 +1,10 @@
+// peguei as informações de hamburguer por meio do query selector e do class hamburguer
+let hamburguer = document.querySelector(".hamburguer"); 
+// aqui pegasse o hamburguer e adiciona um evento no qual se click(função JS) ele adicina uma function
+hamburguer.addEventListener("click", function(){ 
+    // nessa função ele pega o container que é a div que imgloba todo codigo. (.classlist acessa a lista de classes do elemento).
+    //(.toggle adiciona se não tiver ou tira se tiver) o toggle adiciona ou tira a classe show-menu dependendo do click
+    document.querySelector(".container").classList.toggle("show-menu");
+   
+
+});
